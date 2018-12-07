c9f03da8397a78a96773a0acb187f433bbc171d4
Yongwoo Lee <whylee@icon.foundation>
2018-12-07 13:50:24 +0900

    Add a proxy class that provides an Interface SCORE

---
+++ contracts/utility/proxy_score.py
@@ -0,0 +1,50 @@
+from iconservice import *
+
+
+class ProxyScore(type):
+    """
+    A Proxy class that provides an Interface SCORE from the abc class
+
+    usage:
+        token_score = self.create_interface_score(token_address, ProxyScore(ABCIRCToken))
+        token_score.transfer(to, value)
+    """
+
+    @classmethod
+    def _create_class_proxy(mcs, abc_class):
+        """
+        Creates an interface SCORE related in given abc class,
+
+        :param abc_class: An abstract class
+        :return: interface SCORE class
+        """
+
+        interface_functions = {}
+
+        for attribute in abc_class.__dict__.values():
+            if callable(attribute) \
+                    and hasattr(attribute, '__isabstractmethod__') \
+                    and attribute.__isabstractmethod__:
+                attribute.__isabstractmethod__ = False
+                interface_functions[attribute.__name__] = interface(attribute)
+
+        proxy_name = "%s(%s)" % (mcs.__name__, abc_class.__name__)
+        return type(proxy_name, (InterfaceScore, abc_class), interface_functions)
+
+    def __new__(mcs, abc_class):
+        """
+        Retrieves an interface SCORE related in given abc class if the cache exists,
+        otherwise creates a new one
+
+        :param abc_class: An abstract class
+        :return: interface SCORE class
+        """
+        try:
+            cache = mcs.__dict__["_class_proxy_cache"]
+        except KeyError:
+            mcs._class_proxy_cache = cache = {}
+        try:
+            proxy_class = cache[abc_class]
+        except KeyError:
+            cache[abc_class] = proxy_class = mcs._create_class_proxy(abc_class)
+        return proxy_class
