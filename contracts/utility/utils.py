45fe2f9745ba26f51969fb4e7a4a459b0e59ac8b
Yongwoo Lee <whylee@icon.foundation>
2019-01-10 15:43:10 +0900

    Add `require` to `utils.py`

--- contracts/utility/utils.py
+++ contracts/utility/utils.py
@@ -9,7 +9,7 @@ class Utils:
 
     @staticmethod
     def check_valid_address(address: 'Address'):
-        if address == ZERO_SCORE_ADDRESS:
+        if not Utils.is_valid_address(address):
             # todo: write revert message
             revert("")
 
@@ -19,3 +19,17 @@ class Utils:
             # todo: write revert message
             revert("")
 
+    @staticmethod
+    def require(condition: bool, revert_message=None):
+        """
+        Checks the input condition is satisfied. If not satisfied reverts.
+
+        :param condition: condition to check
+        :param revert_message: (Optional) revert message
+        """
+        if not condition:
+            revert(revert_message)
+
+    @staticmethod
+    def is_valid_address(address: 'Address'):
+        return address is not None and address != ZERO_SCORE_ADDRESS
